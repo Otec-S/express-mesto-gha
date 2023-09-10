15c8da3afd66b8e81b096bd125b5becd45757907
Sergey Grigorash <adviser@bk.ru>
2023-09-10 16:59:20 +0500

    feat: connect DB

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+//научили express работать с json
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// подключаемся к серверу mongo
+mongoose
+  .connect("mongodb://127.0.0.1:27017/mestodb", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to DB");
+  });
+
+app.listen(PORT, () => {
+  // Если всё работает, консоль покажет, какой порт приложение слушает
+  console.log(`App listening on port ${PORT}`);
+});
